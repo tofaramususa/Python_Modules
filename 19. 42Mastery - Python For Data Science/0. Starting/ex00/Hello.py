ca3728679703ac2db7443e0ce3b29ea8f153d875
tofaramususa <jtofaramususa@gmail.com>
2024-12-26 12:11:24 +0400

    26_Dec

---
+++ 19. 42Mastery - Python For Data Science/0. Starting/ex00/Hello.py
@@ -0,0 +1,15 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
+
+ft_list[1] = "World!"
+# tuple is immutable
+ft_tuple = ("Hello", "UAE!")
+# set is immutable
+ft_set = {"Hello", "Abu Dhabi!"}
+ft_dict["Hello"] = "42AbuDhabi!"
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
---
+++ 19. 42Mastery - Python For Data Science/0. Starting/ex01/format_ft_time.py
@@ -0,0 +1,8 @@
+# import datetime as datetime
+import time as time
+import calendar as calendar
+
+seconds_since_start = time.gmtime() - time.gmtime(0)
+dateToday = ""
+print(f"Seconds since January 1, 1970: {seconds_since_start} or {seconds_since_start:.2e} in scientific notation")
+print({dateToday})
